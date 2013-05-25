f10820188925f297acab63357ad2873b257d7208
Matthew Price <hello@mattprice.me>
2013-05-24 23:59:46 -0400

    Initial commit
    This is a very rough start, but it's a working example of connecting to
    a Wired server using Golang.

---
+++ main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"runtime"
+	"time"
+)
+
+var (
+	WIRED_SPEC string
+)
+
+// There are a few I/O operations we should perform while the server is starting so
+// that they aren't repeated for each connection we receive. For instance, reading
+// in the Wired specification file for each connection to speed things up later.
+func init() {
+	// Attempt to read in the Wired specification file.
+	file, err := ioutil.ReadFile("WiredSpec_2.0b55.xml")
+
+	// If there's no specification file then we can't continue.
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	WIRED_SPEC = string(file)
+}
+
+type WiredConnection struct {
+	socket     net.Conn
+	serverHost string
+	serverPort int
+}
+
+func (this *WiredConnection) readData() {
+	fmt.Println("Attempting to read data from the socket.")
+
+	result, err := bufio.NewReader(this.socket).ReadString('\r')
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	} else {
+		fmt.Println(string(result))
+	}
+
+}
+
+func (this *WiredConnection) sendAcknowledgement() {
+	fmt.Println("Sending acknowledgement...")
+
+	this.sendTransaction("p7.handshake.acknowledge")
+}
+
+//  Responds to a compatibility check from the server.
+//
+//  Reads in the WiredSpec XML file and sends it to the server. Wired requires that
+//  certain characters be encoded before sending. To save processing time the XML
+//  should be pre-encoded. To save bandwidth the documentation lines should be removed.
+func (this *WiredConnection) sendCompatibilityCheck() {
+	fmt.Println("Sending compatibility check...")
+
+	parameters := map[string]string{
+		"p7.compatibility_check.specification": WIRED_SPEC,
+	}
+	this.sendTransaction("p7.compatibility_check.specification", parameters)
+	this.readData()
+}
+
+// Sends information about the Wired client to the server.
+//
+// For now this is reporting information about the newest known Mac build.
+// In the future, this should report the same information as the Wired version
+// that's connecting to the Push server.
+func (this *WiredConnection) sendClientInformation() {
+	fmt.Println("Sending client information...")
+
+	parameters := map[string]string{
+		"wired.info.application.name":    "Wired Client",
+		"wired.info.application.version": "2.0",
+		"wired.info.application.build":   "268",
+		"wired.info.os.name":             "Mac OS X",
+		"wired.info.os.version":          "10.8.3",
+		"wired.info.arch":                "x86_64",
+		"wired.info.supports_rsrc":       "false",
+	}
+
+	this.sendTransaction("wired.client_info", parameters)
+	this.readData()
+}
+
+// Sends a users login information to the Wired server.
+//
+// The password must be converted to a SHA1 digest before sending it to this function.
+func (this *WiredConnection) sendLogin(user, password string) {
+	fmt.Println("Sending login information...")
+
+	// Send the user login information to the Wired server.
+	parameters := map[string]string{
+		"wired.user.login":    user,
+		"wired.user.password": password,
+	}
+
+	this.sendTransaction("wired.send_login", parameters)
+	this.readData()
+}
+
+func (this *WiredConnection) setNick(nick string) {
+	fmt.Println("Attempting to change nick.")
+
+	parameters := map[string]string{
+		"wired.user.nick": nick,
+	}
+
+	this.sendTransaction("wired.user.set_nick", parameters)
+	this.readData()
+}
+
+func (this *WiredConnection) joinChannel(channel string) {
+	fmt.Printf("Attempting to join channel %s.\n", channel)
+
+	// TODO: Reset the user list for this channel.
+
+	// Attempt to join the channel.
+	parameters := map[string]string{
+		"wired.chat.id": channel,
+	}
+
+	this.sendTransaction("wired.chat.join_chat", parameters)
+	this.readData()
+}
+
+func (this *WiredConnection) sendTransaction(transaction string, parameters ...map[string]string) {
+	// Begin translating the transaction message into XML.
+	generatedXML := `<?xml version="1.0" encoding="UTF-8"?>`
+	generatedXML += fmt.Sprintf(`<p7:message name="%s" xmlns:p7="http://www.zankasoftware.com/P7/Message">`, transaction)
+
+	// If parameters were sent convert them to XML too.
+	if parameters != nil {
+		for key, value := range parameters[0] {
+			generatedXML += fmt.Sprintf(`<p7:field name="%s">%s</p7:field>`, key, value)
+		}
+	}
+
+	// End the transaction message.
+	// Line break is the end message signal for the socket.
+	generatedXML += "</p7:message>\r\n"
+
+	// fmt.Println(generatedXML)
+
+	// Write the data to the socket.
+	_, err := this.socket.Write([]byte(generatedXML))
+
+	// TODO: We need to find something better than this fatal error.
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing data to socket: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func (this *WiredConnection) connectToServer(server string, port int) {
+	timeout, _ := time.ParseDuration("15s")
+
+	// Store the connection info so that we can reconnect later if necessary.
+	this.serverHost = server
+	this.serverPort = port
+
+	// Attempt a socket connection to the server.
+	fmt.Println("Beginning socket connection...")
+	socket, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", server, port), timeout)
+	this.socket = socket
+
+	// If the connection failed, log the error and exit the program.
+	// TODO: This isn't a longterm solution but it will help us get off the ground.
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Connection error: %s", err.Error())
+		os.Exit(1)
+	}
+
+	// Start sending Wired connection info.
+	fmt.Println("Sending Wired handshake...")
+	parameters := map[string]string{
+		"p7.handshake.version":          "1.0",
+		"p7.handshake.protocol.name":    "Wired",
+		"p7.handshake.protocol.version": "2.0",
+	}
+	this.sendTransaction("p7.handshake.client_handshake", parameters)
+	this.readData()
+
+	this.sendAcknowledgement()
+
+	// TODO: Make sure "p7.handshake.compatibility_check" is 1 before sending the compatibility check.
+	this.sendCompatibilityCheck()
+
+	// TODO: Make sure "p7.compatibility_check.status" is 1.
+	// If it's not, we need to end the cancel the connection.
+	this.sendClientInformation()
+
+	this.sendLogin("guest", "da39a3ee5e6b4b0d3255bfef95601890afd80709")
+
+	// TODO: We need to check and see if the login information was correct.
+	this.setNick("Wired Push Server")
+
+	this.joinChannel("1")
+
+	// Close the socket connection.
+	this.socket.Close()
+}
+
+func main() {
+	connection := new(WiredConnection)
+	connection.connectToServer("chat.embercode.com", 2359)
+}
