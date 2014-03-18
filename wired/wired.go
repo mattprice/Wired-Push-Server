2680fa6f56b95d28059ffa224dee27f373f33f46
Matthew Price <hello@mattprice.me>
2014-03-18 19:47:19 -0400

    Update the latest Wired for Mac version numbers.

--- wired/wired.go
+++ wired/wired.go
@@ -229,10 +229,10 @@ func (this *Connection) sendClientInformation() {
 
 	parameters := map[string]string{
 		"wired.info.application.name":    "Wired Client",
-		"wired.info.application.version": "2.0.1",
-		"wired.info.application.build":   "269",
+		"wired.info.application.version": "2.1",
+		"wired.info.application.build":   "306",
 		"wired.info.os.name":             "Mac OS X",
-		"wired.info.os.version":          "10.8.4",
+		"wired.info.os.version":          "10.9.2",
 		"wired.info.arch":                "x86_64",
 		"wired.info.supports_rsrc":       "false",
 	}
